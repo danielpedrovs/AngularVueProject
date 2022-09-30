b432644c6ac6ea5725778df5ef9af09e25ef84ce
danielpedrovs <danielpedrovs@gmail.com>
2022-09-30 23:36:48 +0100

    set goals to 2023

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl=document.querySelector('ul');
+Vue.createApp({
+    data(){
+        return{
+            goals: [],
+            enteredValue:''
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
 
-function addGoal(){
+    }
+}).mount('#app');
 
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl=document.querySelector('ul');
 
-buttonEl.addEventListener('click', );
+// function addGoal(){   
+//      const enteredValue = inputEl.value;
+//      const listItemEl = document.createElement('li');
+//      listItemEl.textContent = enteredValue;
+//      listEl.appendChild(listItemEl);
+//      inputEl.value='';
+
+// }
+
+// buttonEl.addEventListener('click', addGoal );
