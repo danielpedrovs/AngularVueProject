c7afd84c7eec54042b52aa4c7fb9aaff22791456
danielpedrovs <danielpedrovs@gmail.com>
2022-10-01 00:44:10 +0100

    third commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 Vue.createApp({
-    data(){
-        return{
-            goals: [],
-            enteredValue:''
-        };
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods:{
-        addGoal(){
-            this.goals.push(this.enteredValue);
-        }
-
-    }
-}).mount('#app');
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl=document.querySelector('ul');
 
-// function addGoal(){   
+// function addGoal(){
 //      const enteredValue = inputEl.value;
 //      const listItemEl = document.createElement('li');
 //      listItemEl.textContent = enteredValue;
